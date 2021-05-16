26ca3c63145d47dbf18c6c3384a8e3913f5709bf
moriel569 <moriel569@gmail.com>
2021-05-16 22:24:44 +0300

    add basic user service implementation

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,32 @@ const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
   res.json(users.map(User.toResponse));
+});
+
+router.route('/:id').get(async (req, res) => {
+const {id} = req.params;
+const user = await usersService.getById(id);
+res.json(User.toResponse(user));
+})
+
+router.route('/').post(async (req, res) => {
+  const userInfo = req.body
+  const createdUser = await usersService.createUser(userInfo);
+  res.json(User.toResponse(createdUser));
+});
+
+router.route('/:id').put(async (req, res) => {
+  const {id} = req.params;
+  const userInfo = req.body;
+  const updatedUser = await usersService.updateUser(id,userInfo);
+  res.json(User.toResponse(updatedUser));
+});
+
+router.route('/:id').delete(async (req, res) => {
+  const {id} = req.params;
+  await usersService.deleteUser(id);
+  res.json();
 });
 
 module.exports = router;
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -2,4 +2,12 @@ const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const getById = id => usersRepo.getById(id);
+
+const createUser = user => usersRepo.createUser(user);
+
+const updateUser = (id,user) => usersRepo.updateUser(id,user);
+
+const deleteUser = id => usersRepo.deleteUser(id);
+
+module.exports = { getAll, getById, createUser,updateUser,deleteUser };
